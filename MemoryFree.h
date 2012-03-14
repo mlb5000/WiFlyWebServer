ec5a72abf8cc4fdacaaa09d3fabae53a08a03deb
Matthew Baker <github@bakermatt.com>
2012-03-14 19:03:29 -0400

    Updating MemoryFree module

--- MemoryFree.h
+++ MemoryFree.h
@@ -1,4 +1,7 @@
-// memoryFree header
+// MemoryFree library based on code posted here:
+// http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1213583720/15
+// 
+// Extended by Matthew Murdoch to include walking of the free list.
 
 #ifndef	MEMORY_FREE_H
 #define MEMORY_FREE_H
